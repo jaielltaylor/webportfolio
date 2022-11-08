f93ae0604e5f8fc26519a100fbf1d046a5f85a7c
unknown <jay.noble.taylor@gmail.com>
2022-11-07 19:16:46 -0500

    bigUpdate

--- js-files/main.js
+++ js-files/main.js
@@ -21,25 +21,13 @@ function scrollToPersonal()
 {
     window.scrollTo
     ({
-        top: 5500, /*subject to change*/
+        top: 6000, /*subject to change*/
         behavior: 'smooth'
     });
 }
 
 $(document).ready(function () {
     $('#open-contact').on('click', function()
-    {
-        $('#contact-holder').fadeIn();
-        $('body').addClass('overflow-hidden');
-    });
-
-    $('.go-back').on('click', function()
-    {
-        $('.popup').fadeOut();
-        $('body').removeClass('overflow-hidden');
-    });
-
-    $('#open-contact2').on('click', function()
     {
         $('#contact-holder').fadeIn();
         $('body').addClass('overflow-hidden');
